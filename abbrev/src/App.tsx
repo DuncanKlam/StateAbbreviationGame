2859aaf32129fb88645c2291f9904e9281231736
Duncan Klemm <duncan.klemm141@gmail.com>
2023-10-10 20:37:55 -0400

    fix easy options

--- abbrev/src/App.tsx
+++ abbrev/src/App.tsx
@@ -25,6 +25,7 @@ export enum GameMode {
 function App() {
 
   const [gameData, setGameData] = useState<GData[]>([])
+  const [easyOptions, setEasyOptions] = useState<string[]>([])
   const [playstyle, setPlaystyle] = useState<PlayStyle>(PlayStyle.UNCHOSEN)
   const [gamemode, setGamemode] = useState<GameMode>(GameMode.UNCHOSEN)
   const [isSetupButtonsVisible, setIsSetupButtonsVisible] = useState(false)
@@ -74,8 +75,8 @@ function App() {
             {
               (playstyle === PlayStyle.EASY) && 
               <div className="button-row">
-                {generateOptions(gameData[currentIndex]).map((opt, index) => (
-                  <button className={`${gamemode === GameMode.EASY && ''}`} key={index} onClick={handleOptionClick}>{opt}</button>
+                {easyOptions.map((opt, index) => (
+                  <button className={`${inputVal === opt && 'selected'}`} key={index} onClick={() => handleOptionClick(opt)}>{opt}</button>
                 ))}
               </div>
             }
@@ -96,7 +97,7 @@ function App() {
               <span id="correct">{resultsData}</span>
               <span id="incorrect">{50 - resultsData}</span>
             </div>
-            <p>{resultsData >= 45 ? "So close, great job!" : resultsData >= 35 ? "You did good!" : "oof :( try again"}</p>
+            <p>{resultsData === 50 ? "Excellent!" : resultsData >= 45 ? "So close, great job!" : resultsData >= 35 ? "You did good!" : "oof :( try again"}</p>
             <div className="button-row">
               <button onClick={backToStart}>Back to Start</button>
             </div>
@@ -113,29 +114,32 @@ function App() {
   function gamemodeHard(){setGamemode(GameMode.HARD)}
   function gamemodeGodly(){setGamemode(GameMode.GODLY)}
   function handleInputChange(e: React.FormEvent<HTMLInputElement>){ setInputVal(e.currentTarget.value.toUpperCase()) }
-  function handleOptionClick(e: React.FormEvent<HTMLButtonElement>){ setInputVal(e.currentTarget.value)}
+  function handleOptionClick(e: string){ setInputVal(e) }
   function checkSubmit(e: any){if(e && e.keyCode === 13){submitAnswer()}}
 
   function startGame(){
-    if (gamemode === GameMode.EASY) { createEasyGameData() } 
-    else if (gamemode === GameMode.HARD) { createHardGameData() } 
-    else { createGodlyGameData() }
+    let gameData: GData[] = [];
+    if (gamemode === GameMode.EASY) { gameData = createEasyGameData() } 
+    else if (gamemode === GameMode.HARD) { gameData = createHardGameData() } 
+    else { gameData = createGodlyGameData() }
+    setGameData(gameData)
     setIsSetupButtonsVisible(false)
     setCurrentIndex(0)
+    setEasyOptions(generateOptions(gameData[0]))
     setShowGame(true)
   }
 
   function createEasyGameData(){
-    setGameData(GameData.map((stateData, index) => ({...stateData, completed: false, guess: "", order: index })).sort((a,b) => a.order - b.order))
+    return GameData.map((stateData, index) => ({...stateData, completed: false, guess: "", order: index })).sort((a,b) => a.order - b.order)
   }
 
   function createHardGameData(){
     let stateNums = [...Array(50).keys()]
-    setGameData(GameData.map((stateData) => {
+    return GameData.map((stateData) => {
       const randomStateNum = stateNums[Math.floor(Math.random()*stateNums.length)];
       stateNums = stateNums.filter( num => num !== randomStateNum )
       return ({ ...stateData, completed: false, guess: "", order: randomStateNum })
-    }).sort((a,b) => a.order - b.order))
+    }).sort((a,b) => a.order - b.order)
   }
 
   function createGodlyGameData(){
@@ -161,6 +165,7 @@ function App() {
       setShowResults(true)
       setCurrentIndex(0)
     } else {
+    if (playstyle === PlayStyle.EASY) setEasyOptions(generateOptions(gameData[currentIndex + 1]))
       setCurrentIndex(currentIndex + 1)
     }
     document.getElementById("abbr-input")?.focus();
@@ -184,7 +189,6 @@ function App() {
   }
 
   function generateOptions({ state, abbr }: GData){
-    debugger
     const normalized = state.toUpperCase().replaceAll(" ", "")
     let answers = [abbr]
     while (answers.length < 3) {
